ba393a143b93ad0bcf9582c5803485489920be73
Sp0k <contact@gabsavard.com>
2025-07-17 03:06:02 -0300

    feat(dashboard): added create modal for files and folders
    - Created modals for both Creating new files and Creating new folders
    - Added new translations to locales
    - Updated the dashboard for the modals

--- ui/src/components/DashboardComponents/SubBar/SubBar.jsx
+++ ui/src/components/DashboardComponents/SubBar/SubBar.jsx
@@ -3,13 +3,13 @@ import { useTranslation } from 'react-i18next'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFileUpload, faFileAlt, faFolderPlus } from '@fortawesome/free-solid-svg-icons'
 
-const SubBar = () => {
+const SubBar = ({ setIsCreateFolderModalOpen, setIsCreateFileModalOpen }) => {
   const { t } = useTranslation();
   return (
     <nav className="navbar navbar-expand-lg mt-2 navbar-light bg-white px-5 py-2">
       <p>{t('root')}</p>
 
-      <ul className="navbar-nav ms-auto">
+      <ul className="navbar-nav ms-auto me-5 mb-3">
         <li className="nav-item mx-2">
           <button className="btn btn-outline-dark">
             <FontAwesomeIcon icon={faFileUpload} /> &nbsp;
@@ -17,13 +17,19 @@ const SubBar = () => {
           </button>
         </li>
         <li className="nav-item mx-2">
-          <button className="btn btn-outline-dark">
+          <button 
+            className="btn btn-outline-dark" 
+            onClick={() => setIsCreateFileModalOpen(true)}
+          >
             <FontAwesomeIcon icon={faFileAlt} /> &nbsp;
             {t('create.file')}
           </button>
         </li>
         <li className="nav-item mx-2">
-          <button className="btn btn-outline-dark">
+          <button 
+            className="btn btn-outline-dark" 
+            onClick={() => setIsCreateFolderModalOpen(true)}
+          >
             <FontAwesomeIcon icon={faFolderPlus} /> &nbsp;
             {t('create.folder')}
           </button>
--- ui/src/pages/DashboardPage/DashboardPage.jsx
+++ ui/src/pages/DashboardPage/DashboardPage.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react"
 import HomeComponent from "../../components/DashboardComponents/HomeComponent/HomeComponent"
 import Navbar from "../../components/DashboardComponents/Navbar/Navbar"
 import SubBar from "../../components/DashboardComponents/SubBar/SubBar"
+import CreateFolder from "../../components/DashboardComponents/CreateFolder/CreateFolder"
+import CreateFile from "../../components/DashboardComponents/CreateFile/CreateFile"
 
 const DashboardPage = () => {
+  const [isCreateFolderModalOpen, setIsCreateFolderModalOpen] = useState(false);
+  const [isCreateFileModalOpen, setIsCreateFileModalOpen] = useState(false);
+
   return (
     <>
+      {
+        isCreateFolderModalOpen && (
+          <CreateFolder
+            setIsCreateFolderModalOpen={setIsCreateFolderModalOpen}
+          />
+        )
+      }
+      {
+        isCreateFileModalOpen && (
+          <CreateFile
+            setIsCreateFileModalOpen={setIsCreateFileModalOpen}
+          />
+        )
+      }
       <Navbar />
-      <SubBar />
+      <SubBar 
+        setIsCreateFolderModalOpen={setIsCreateFolderModalOpen} 
+        setIsCreateFileModalOpen={setIsCreateFileModalOpen}
+      />
       <HomeComponent />
     </>
   )
